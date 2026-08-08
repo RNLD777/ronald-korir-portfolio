efe49932549a1c14ac5418ade290fb146f3ae723
Ronald Korir <ronaldkiplangat8690@gmail.com>
2026-08-08 13:49:13 +0300

    Fix article cover image loading

--- lib/notion/writing.ts
+++ lib/notion/writing.ts
@@ -6,22 +6,31 @@ import { mapWriting } from "./map-writing";
 const DATA_SOURCE_ID = "23ffc6b9-7792-490e-b8ec-100c402df5d6";
 
 async function getFirstImage(pageId: string) {
-  const blocks = await notion.blocks.children.list({
-    block_id: pageId,
-    page_size: 20,
-  });
+  let cursor: string | undefined = undefined;
 
-  const image = blocks.results.find(
-    (block: any) => block.type === "image"
-  ) as any;
+  do {
+    const response = await notion.blocks.children.list({
+      block_id: pageId,
+      page_size: 100,
+      start_cursor: cursor,
+    });
 
-  if (!image) return "/placeholder.jpg";
+    const image = response.results.find(
+      (block: any) => block.type === "image"
+    ) as any;
 
-  if (image.image.type === "external") {
-    return image.image.external.url;
-  }
+    if (image) {
+      if (image.image.type === "external") {
+        return image.image.external.url;
+      }
 
-  return image.image.file.url;
+      return image.image.file.url;
+    }
+
+    cursor = response.has_more ? response.next_cursor ?? undefined : undefined;
+  } while (cursor);
+
+  return "/placeholder.jpg";
 }
 
 async function fetchWriting() {
@@ -45,25 +54,10 @@ async function fetchWriting() {
 
 export const getWriting = unstable_cache(
   async () => {
-    const response = await notion.dataSources.query({
-      data_source_id: DATA_SOURCE_ID,
-      page_size: 100,
-    });
-
-    const articles = await Promise.all(
-      response.results.map(async (page: any) => {
-        const article = mapWriting(page);
-
-        article.image = await getFirstImage(page.id);
-
-        return article;
-      })
-    );
-
-    return articles;
+    return fetchWriting();
   },
   ["writing"],
   {
-    revalidate: 86400, // 24 hours
+    revalidate: 86400,
   }
 );
