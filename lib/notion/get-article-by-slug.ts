c0575b0acf7b7cf2294355a6fd996d4251744a3a
Ronald Korir <ronaldkiplangat8690@gmail.com>
2026-08-10 13:19:29 +0300

    Cache article lookups

--- lib/notion/get-article-by-slug.ts
+++ lib/notion/get-article-by-slug.ts
@@ -1,15 +1,29 @@
+import { unstable_cache } from "next/cache";
+
 import { notion } from "./client";
 import { mapWriting } from "./map-writing";
 
 const DATA_SOURCE_ID = "23ffc6b9-7792-490e-b8ec-100c402df5d6";
 
-export async function getArticleBySlug(slug: string) {
+async function fetchArticleBySlug(slug: string) {
   const response = await notion.dataSources.query({
     data_source_id: DATA_SOURCE_ID,
     page_size: 100,
   });
 
-  const articles = response.results.map((page: any) => mapWriting(page));
+  const article = response.results
+    .map((page: any) => mapWriting(page))
+    .find((article) => article.slug === slug);
 
-  return articles.find((article) => article.slug === slug) ?? null;
+  return article ?? null;
+}
+
+export async function getArticleBySlug(slug: string) {
+  return unstable_cache(
+    () => fetchArticleBySlug(slug),
+    ["article-by-slug", slug],
+    {
+      revalidate: 86400,
+    }
+  )();
 }
